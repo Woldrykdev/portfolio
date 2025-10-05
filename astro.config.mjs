6e4df940cdd6d47ff07fab73921c93039f74407a
Esteban Osen <woldryk@Woldryks-MacBook-Air.local>
2025-10-05 20:50:38 -0300

    asd

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   output: 'server',
