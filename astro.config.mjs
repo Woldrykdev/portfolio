81448947104523f41c79a9e0aceeed488e3cfb54
Esteban Osen <woldryk@Woldryks-MacBook-Air.local>
2025-10-05 20:54:08 -0300

    asd

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,3 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
-
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
--- src/server/db/client.ts
+++ src/server/db/client.ts
@@ -1,5 +1,7 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 
-const sql = postgres(process.env.DATABASE_URL!, { ssl: { rejectUnauthorized: false } });
+const sql = postgres(process.env.DATABASE_URL!, {
+  ssl: { rejectUnauthorized: false },
+});
 export const db = drizzle(sql);
