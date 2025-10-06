cfe5ac519d54f23b2bf45981a4fb86424c3b9827
Esteban Osen <woldryk@Woldryks-MacBook-Air.local>
2025-10-05 21:40:21 -0300

    asd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
+  content: [
+    './src/**/*.{astro,js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {},
   },
